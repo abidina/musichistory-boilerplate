4cba86f5d6a1041d933d4d67d36798be3b1e474e
Abbie Holder <abigail.c.holder@gmail.com>
2016-04-11 20:40:29 -0500

    updating musichistory

---
+++ songs.js
@@ -0,0 +1,6 @@
+var songs = [];
+
+songs.unshift("'Please Pardon Yourself' by The Avett Brothers on the album Mignonette");
+songs.push("'Agape' by Bear's Den on the album Agape");
+console.log(songs);
+
