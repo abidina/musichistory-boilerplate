2c1c227963039cdce6a491fa752d5ef8d8cc15a2
Abbie Holder <abigail.c.holder@gmail.com>
2016-04-30 12:00:23 -0500

    highly unpresentable, but functioning

--- songs.js
+++ songs.js
@@ -1,6 +1,25 @@
 var songs = [];
+var songElement = document.getElementById('songSection');
 
-songs.unshift("'Please Pardon Yourself' by The Avett Brothers on the album Mignonette");
-songs.push("'Agape' by Bear's Den on the album Agape");
-console.log(songs);
+songs[songs.length] = "Legs > by Z*ZTop on the album Eliminator";
+songs[songs.length] = "The Logical Song > by Supertr@amp on the album Breakfast in America";
+songs[songs.length] = "Another Brick in the Wall > by Pink Floyd on the album The Wall";
+songs[songs.length] = "Welco(me to the Jungle > by Guns & Roses on the album Appetite for Destruction";
+songs[songs.length] = "Ironi!c > by Alanis Moris*ette on the album Jagged Little Pill";
 
+songs.unshift("Please Pardon Yourself = by The Avett Brothers on the album Mignonette");
+songs.push("Agape = by Bear's Den on the album Agape");
+
+var newSongs = [];
+    songs = songs.toString().replace(/>/g, "=");
+    songs = songs.replace("@", "");
+    songs = songs.replace("(", "");
+    songs = songs.replace("*", "");
+    songs = songs.replace("*e", "e");
+    songs = songs.replace("!", "").split(',');
+
+for (var i = 0; i < songs.length; i++) {
+  var string = "";
+  var string = "<p>" + songs[i] + "</p>";
+  songElement.innerHTML += string;
+};
